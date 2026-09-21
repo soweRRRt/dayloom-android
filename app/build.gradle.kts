fd3e82abdd9a9115a470ad0f07593260276f2795
soweRRRt <soweRRRt@users.noreply.github.com>
2026-09-21 23:59:21 +0400

    chore: prepare v0.3.1 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.sowerrrt.dayloom"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.3.0"
+        versionCode = 4
+        versionName = "0.3.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
